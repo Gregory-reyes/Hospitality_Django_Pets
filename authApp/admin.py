dfde083aa39616c7c8c2011e7d3f8b1402e20828
Gregory Reyes <greyes3@udi.edu.co>
2023-05-02 11:48:21 -0500

    Migración de base de datos creada en los models Hospitality

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models.usuario import Usuario
+from .models.signos_vitales import Signos_vitales
+from .models.paciente import Paciente
+from .models.personal_de_salud import Personal_salud
+from .models.familiar import Familiar
+from .models.historia_clinica import Historia_clinica
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Signos_vitales)
+admin.site.register(Paciente)
+admin.site.register(Personal_salud)
+admin.site.register(Familiar)
+admin.site.register(Historia_clinica)
